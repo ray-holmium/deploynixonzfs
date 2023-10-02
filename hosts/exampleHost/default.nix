bda29f4158492cebae9749495468e1309a1b6b0b
ray-holmium <ray-holmium@mailbox.org>
2023-10-02 08:06:08 +0000

    working on boot disk reference
    Signed-off-by: ray-holmium <ray-holmium@mailbox.org>

--- hosts/exampleHost/default.nix
+++ hosts/exampleHost/default.nix
@@ -9,8 +9,8 @@
   inherit pkgs system;
   zfs-root = {
     boot = {
-      devNodes = "/dev/";
-      bootDevices = [ "nvme0n1p2/mnt/bios" ];
+      devNodes = "/dev/disk/by-id";
+      bootDevices = [ "nvme-ADATA_SX8200PNP_2K1520130319" ];
       immutable = false;
       availableKernelModules = [ "kernelModules_placeholder" ];
       removableEfi = true;
